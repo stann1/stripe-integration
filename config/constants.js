95cead1aabe8326bdf11badc818fad1cfa1b2609
Stanislav Stoychev <stoychev.st2@gmail.com>
2019-11-05 16:41:42 +0200

    added env config for URL; added customers view

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+const DEFAULT_PUBLIC_URL = "https://fd7bf1bc.ngrok.io";
 
 const getSecretKey = (name) => {
     if(name === "stripe" && fs.existsSync("secret.key")){
@@ -14,9 +15,10 @@ module.exports = {
     STRIPE_SECRET_KEY: getSecretKey("stripe"),
     STRIPE_WEBHOOK_KEY: getSecretKey("stripe-webhook"),
     STRIPE_PUBLIC_KEY: "pk_test_OTiVCeCxAZjtkvoAG2kZ102S00hXMZ3ztu",
-    PUBLIC_URL: "https://fd7bf1bc.ngrok.io",
+    PUBLIC_URL: process.env.NGROK || DEFAULT_PUBLIC_URL,
     PLANS: {
         Pro_Month: "plan_G51LZkrmacfT8p",
         Pro_Year: "plan_G5kyCpqeORB2Uh"
-    }
+    },
+    DEFAULT_PUBLIC_URL
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const {STRIPE_SECRET_KEY} = require('../config/constants');
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
+var debug = require('debug')('app:usersroute');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+
+  try {
+    const customers = await stripe.customers.list(
+      { limit: 10 }
+      );
+    //const subscriptions = [{id: 1, name: "name"}, {id: 2, name: "name2"}];
+    let subContent = [];
+    const result = Object.assign({}, customers, {data: null});
+    debug(result);
+  
+    if(customers && customers.data){
+      subContent = customers.data.map(s => JSON.stringify(s));
+    }
+  
+    res.render('users', {customers: subContent});
+  } catch (error) {
+    next(error);
+  }  
 });
+
+router.get('/:id/cancel', async (req,res,next) => {
+
+  res.render('users', {message: "Subscription cancelled"});
+})
 
 module.exports = router;
