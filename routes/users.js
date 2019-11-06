38d1c9a02ffbd5de01992f2a86a2fd844046fc09
Stann <stoychev.st2@gmail.com>
2019-11-06 21:36:20 +0200

    minor improvements to users

--- routes/users.js
+++ routes/users.js
@@ -6,18 +6,27 @@ var debug = require('debug')('app:usersroute');
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
+  res.render('users', {customers: []});
+});
 
+router.get('/all', async (req, res, next) => {
   try {
     const customers = await stripe.customers.list(
       { limit: 10 }
-      );
-    //const subscriptions = [{id: 1, name: "name"}, {id: 2, name: "name2"}];
+    );
+    
     let subContent = [];
     const result = Object.assign({}, customers, {data: null});
     debug(result);
   
     if(customers && customers.data){
-      subContent = customers.data.map(s => JSON.stringify(s));
+      subContent = customers.data.map(cus => {
+        let subscriptions = [];
+        if (cus.subscriptions && cus.subscriptions.data) {
+          subscriptions = cus.subscriptions.data.map(sub => sub.id);
+        }
+        return {id: cus.id, email: cus.email, subscriptions, data: JSON.stringify(cus)}
+      });
     }
   
     res.render('users', {customers: subContent});
@@ -27,8 +36,20 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:id/cancel', async (req,res,next) => {
+  debug("Cancelling subscription for user: " + req.params.id);
+  res.render('users', {message: "All Subscriptions cancelled for customer " + req.params.id});
+})
 
-  res.render('users', {message: "Subscription cancelled"});
+router.get('/cancelsubscription/:subid', async (req,res,next) => {
+  const subId = req.params.subid;
+  debug("Cancelling subscription: " + subId);
+  try {
+    const result = await stripe.subscriptions.del(subId);
+    debug(result)
+    res.render('users', {message: `Subscription ${subId} cancelled`});
+  } catch (error) {
+    return next(error);
+  }
 })
 
 module.exports = router;
